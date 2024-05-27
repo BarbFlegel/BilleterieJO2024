7f4540e867710ec3d073a080b67765a173e5f71c
Fleg <barbora.flegelova@studi.fr>
2024-05-27 14:46:45 +0200

    Assembly update

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84b67eaffd13f503995c4f9eab05e5a2dbeb3822")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+395191ab8659ecb6d60944b8c4f4ab06145c40b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
