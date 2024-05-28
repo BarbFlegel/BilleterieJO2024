71bd86cda96142a5b08259f981d7f5b2de2111f2
Fleg <barbora.flegelova@studi.fr>
2024-05-28 15:43:25 +0200

    deploy conf

--- API/Program.cs
+++ API/Program.cs
@@ -45,31 +45,31 @@ builder.Services.AddSwaggerGen(c =>
     });
 });
 
-// string connString;
-// if (builder.Environment.IsDevelopment())
-//     connString = builder.Configuration.GetConnectionString("DefaultConnection");
-// else
-// {
-//     // Use connection string provided at runtime by FlyIO.
-//     var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+string connString;
+if (builder.Environment.IsDevelopment())
+    connString = builder.Configuration.GetConnectionString("DefaultConnection");
+else
+{
+    // Use connection string provided at runtime by FlyIO.
+    var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
 
-//     // Parse connection URL to connection string for Npgsql
-//     connUrl = connUrl.Replace("postgres://", string.Empty);
-//     var pgUserPass = connUrl.Split("@")[0];
-//     var pgHostPortDb = connUrl.Split("@")[1];
-//     var pgHostPort = pgHostPortDb.Split("/")[0];
-//     var pgDb = pgHostPortDb.Split("/")[1];
-//     var pgUser = pgUserPass.Split(":")[0];
-//     var pgPass = pgUserPass.Split(":")[1];
-//     var pgHost = pgHostPort.Split(":")[0];
-//     var pgPort = pgHostPort.Split(":")[1];
-//     var updatedHost = pgHost.Replace("flycast", "internal");
+    // Parse connection URL to connection string for Npgsql
+    connUrl = connUrl.Replace("postgres://", string.Empty);
+    var pgUserPass = connUrl.Split("@")[0];
+    var pgHostPortDb = connUrl.Split("@")[1];
+    var pgHostPort = pgHostPortDb.Split("/")[0];
+    var pgDb = pgHostPortDb.Split("/")[1];
+    var pgUser = pgUserPass.Split(":")[0];
+    var pgPass = pgUserPass.Split(":")[1];
+    var pgHost = pgHostPort.Split(":")[0];
+    var pgPort = pgHostPort.Split(":")[1];
+    var updatedHost = pgHost.Replace("flycast", "internal");
 
-//     connString = $"Server={updatedHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
-// }
+    connString = $"Server={updatedHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
+}
 builder.Services.AddDbContext<StoreContext>(opt =>
 {
-    opt.UseNpgsql(builder.Configuration.GetConnectionString("DATABASE_URL"));
+    opt.UseNpgsql(connString);
 });
 
 builder.Services.AddCors();
@@ -125,7 +125,7 @@ app.UseAuthorization();
 app.MapControllers();
 app.MapFallbackToController("Index", "Fallback");
 
-var scope = app.Services.CreateScope();
+using var scope = app.Services.CreateScope();
 var context = scope.ServiceProvider.GetRequiredService<StoreContext>();
 var userManager = scope.ServiceProvider.GetRequiredService<UserManager<User>>();
 var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
