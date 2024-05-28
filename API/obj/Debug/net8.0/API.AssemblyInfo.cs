75f8ec50e76ee3fe4622d13f797846eb55d1f068
Fleg <barbora.flegelova@studi.fr>
2024-05-28 07:47:57 +0200

    commit of gitignore

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+808e90585188e1807eae65a7dc304a2762f8c530")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd2cdcae9eb0250e8ddbf2bc851abcd3a99c714d")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
